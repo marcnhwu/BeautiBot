865ebdc4b064335f56f27279683b39484d8bf612
Marc Wu <nienhengwu@gmail.com>
2021-09-10 00:11:07 +0800

    init commit

---
+++ botREF.py
@@ -0,0 +1,17 @@
+
+# userDefinedDICT
+userDefinedDICT = {"bodyparts": ["毛", "腋", "腋下", "腿", "小腿", "大腿", "膝蓋", "腳", "腳趾", "腳背", "比基尼線", "私密處", "手", "手臂", "上手臂", "下手臂", "全手", "手指", "手背", "臉", "全臉", "鬍子", "眉心", "唇周", "下巴", "頸", "前頸", "後頸", "胸", "胸部", "腹", "腹部", "子母線", "背", "上背", "下背", "臀", "臀部", "乳暈"]}
+
+
+# if we need to clarify which bodypart
+handleBodypartDICT = {"腿": ["大腿還是小腿呢？"],
+                      "手臂": ["上手臂還是下手臂呢？"],
+                      "手": ["手指、手背還是全手呢？"],
+                      "脖子": ["前頸還是後頸呢？"],
+                      "頸部": ["前頸還是後頸呢？"]}
+
+#print(list(["".join(v) for v in handleBodypartDICT.values()]))
+
+#print(userDefinedDICT["bodyparts"])
+
+#print([key for key in handleBodypartDICT.keys()])
