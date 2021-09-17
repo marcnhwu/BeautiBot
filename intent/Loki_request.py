85a1f078e1fd1b344692467c7f790176272bf8ea
Marc Wu <nienhengwu@gmail.com>
2021-09-18 03:05:14 +0800

    add new sample sentences

--- intent/Loki_request.py
+++ intent/Loki_request.py
@@ -15,7 +15,7 @@
 """
 
 DEBUG_request = True
-userDefinedDICT = {"bodyparts": ["毛", "腋", "腋下", "腿", "小腿", "大腿", "膝蓋", "腳", "腳趾", "腳背", "比基尼線", "私密處", "手", "手臂", "上手臂", "下手臂", "全手", "手指", "手背", "臉", "全臉", "鬍子", "眉心", "唇周", "下巴", "頸", "前頸", "後頸", "胸", "胸部", "腹", "腹部", "子母線", "背", "上背", "下背", "臀", "臀部", "乳暈"]}
+userDefinedDICT = {"bodypart": ["毛", "腋", "腋下", "腿", "小腿", "大腿", "膝蓋", "腳", "腳趾", "腳背", "比基尼線", "私密處", "手", "手臂", "上手臂", "下手臂", "全手", "手指", "手背", "臉", "全臉", "鬍子", "眉心", "唇周", "下巴", "頸", "前頸", "後頸", "胸", "胸部", "腹", "腹部", "子母線", "背", "上背", "下背", "臀", "臀部", "乳暈", "胳肢窩"], "location": ["忠孝敦化", "中山"], "doctorName": ["王經凱", "程昭瑞", "劉宇婷", "謝羽翔", "薛博駿", "陳棨揮"], "medicalCondition": ["藥物過敏", "凝血功能障礙", "蟹足腫", "免疫疾病", "糖尿病", "癲癇", "懷孕", "哺乳中", "抗生素"]}
 
 # 將符合句型的參數列表印出。這是 debug 或是開發用的。
 def debugInfo(inputSTR, utterance):
@@ -110,6 +110,34 @@ def getResult(inputSTR, utterance, args, resultDICT):
         resultDICT["bodypart"] = args[0]
         resultDICT["request"] = True 
         resultDICT["confirm"] = ""
+        pass
+
+    if utterance == "[我]想找[程昭瑞]醫師除[腿]毛":
+        resultDICT["bodypart"] = args[2]
+        resultDICT["request"] = True 
+        resultDICT["confirm"] = ""
+        resultDICT["appointmentDoctor"] = args[1]
+        pass
+
+    if utterance == "[我]想找[程昭瑞]醫師除毛":
+        resultDICT["bodypart"] = ""
+        resultDICT["request"] = True 
+        resultDICT["confirm"] = ""
+        resultDICT["appointmentDoctor"] = args[1]
+        pass
+
+    if utterance == "[我]想找[程昭瑞]除[腿]毛":
+        resultDICT["bodypart"] = args[2]
+        resultDICT["request"] = True 
+        resultDICT["confirm"] = ""
+        resultDICT["appointmentDoctor"] = args[1]
+        pass
+
+    if utterance == "[我]想找[程昭瑞]除毛":
+        resultDICT["bodypart"] = ""
+        resultDICT["request"] = True 
+        resultDICT["confirm"] = ""
+        resultDICT["appointmentDoctor"] = args[1]
         pass
 
     return resultDICT
