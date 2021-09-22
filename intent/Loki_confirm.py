22cf3bfc276348f4a03e3f81fa75725fcaa66219
Marc Wu <nienhengwu@gmail.com>
2021-09-22 17:12:12 +0800

    update

--- intent/Loki_confirm.py
+++ intent/Loki_confirm.py
@@ -15,7 +15,7 @@
 """
 
 DEBUG_confirm = True
-userDefinedDICT = {"bodyparts": ["毛", "腋", "腋下", "腿", "小腿", "大腿", "膝蓋", "腳", "腳趾", "腳背", "比基尼線", "私密處", "手", "手臂", "上手臂", "下手臂", "全手", "手指", "手背", "臉", "全臉", "鬍子", "眉心", "唇周", "下巴", "頸", "前頸", "後頸", "胸", "胸部", "腹", "腹部", "子母線", "背", "上背", "下背", "臀", "臀部", "乳暈", "胳肢窩"]}
+userDefinedDICT = {"bodypart": ["毛", "腋", "腋下", "腿", "小腿", "大腿", "膝蓋", "腳", "腳趾", "腳背", "比基尼線", "私密處", "手", "手臂", "上手臂", "下手臂", "全手", "手指", "手背", "臉", "全臉", "鬍子", "眉心", "唇周", "下巴", "頸", "前頸", "後頸", "胸", "胸部", "腹", "腹部", "子母線", "背", "上背", "下背", "臀", "臀部", "乳暈", "胳肢窩"], "location": ["忠孝敦化", "中山"], "medicalCondition": ["藥物過敏", "凝血功能障礙", "蟹足腫", "免疫疾病", "糖尿病", "癲癇", "懷孕", "哺乳中", "抗生素"]}
 
 # 將符合句型的參數列表印出。這是 debug 或是開發用的。
 def debugInfo(inputSTR, utterance):
@@ -25,19 +25,33 @@ def debugInfo(inputSTR, utterance):
 def getResult(inputSTR, utterance, args, resultDICT):
     debugInfo(inputSTR, utterance)
     if utterance == "不好":
-        resultDICT["confirm"] = False
+        if "不對" in inputSTR:
+            resultDICT["confirm"] = False
+        else:
+            resultDICT["confirm"] = False
         pass
 
     if utterance == "不是":
-        resultDICT["confirm"] = False
+        if "不是的" in inputSTR:
+            resultDICT["confirm"] = False
+        else:
+            resultDICT["confirm"] = False
         pass
 
     if utterance == "不確定":
-        resultDICT["confirm"] = False
+        if "不正確" in inputSTR:
+            resultDICT["confirm"] = False
+        else:
+            resultDICT["confirm"] = False
         pass
 
     if utterance == "好":
-        resultDICT["confirm"] = True
+        if "正確" in inputSTR:
+            resultDICT["confirm"] = True
+        elif "對" in inputSTR:
+            resultDICT["confirm"] = True
+        else:
+            resultDICT["confirm"] = True
         pass
 
     if utterance == "好的":
@@ -45,7 +59,10 @@ def getResult(inputSTR, utterance, args, resultDICT):
         pass
 
     if utterance == "恩恩":
-        resultDICT["confirm"] = True
+        if "恩" in inputSTR or "嗯" in inputSTR:
+            resultDICT["confirm"] = True
+        else:
+            resultDICT["confirm"] = True
         pass
 
     if utterance == "摁":
@@ -53,11 +70,17 @@ def getResult(inputSTR, utterance, args, resultDICT):
         pass
 
     if utterance == "摁摁":
-        resultDICT["confirm"] = True
+        if "嗯嗯" in inputSTR:
+            resultDICT["confirm"] = True
+        else:
+            resultDICT["confirm"] = True
         pass
 
     if utterance == "是":
-        resultDICT["confirm"] = True
+        if "是的" in inputSTR:
+            resultDICT["confirm"] = True
+        else:
+            resultDICT["confirm"] = True
         pass
 
     if utterance == "有":
